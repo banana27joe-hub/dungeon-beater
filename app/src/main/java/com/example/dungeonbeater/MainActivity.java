b782ceed37e3b25dd0c6dedb0b2ecaed090675c7
banana27joe-hub <banana27joe@gmail.com>
2026-08-22 19:52:44 +0500

    Create MainActivity.java

---
+++ app/src/main/java/com/example/dungeonbeater/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.dungeonbeater;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    private GameView gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        gameView = new GameView(this);
+        setContentView(gameView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.pause();
+    }
+}
