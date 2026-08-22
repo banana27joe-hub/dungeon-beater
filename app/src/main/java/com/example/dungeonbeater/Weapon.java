98130c3f080b1e4bc3926069a1469cf8b420413f
banana27joe-hub <banana27joe@gmail.com>
2026-08-22 19:57:13 +0500

    Create Weapon.java

---
+++ app/src/main/java/com/example/dungeonbeater/Weapon.java
@@ -0,0 +1,39 @@
+package com.example.dungeonbeater;
+
+public abstract class Weapon {
+
+    public enum Type {
+        MELEE,
+        RANGED
+    }
+
+    protected String name;
+    protected Type type;
+    protected int damage;
+    protected float attackCooldown;
+
+    public Weapon(String name, Type type, int damage, float attackCooldown) {
+        this.name = name;
+        this.type = type;
+        this.damage = damage;
+        this.attackCooldown = attackCooldown;
+    }
+
+    public abstract void performAttack(Player owner);
+
+    public Type getType() {
+        return type;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public float getAttackCooldown() {
+        return attackCooldown;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
